6608db297fdfe7cc435076f55455ec1ae5ecd9c5
pomadev <y.pomadev@gmail.com>
2019-10-12 01:51:45 +0900

    Add関数実装

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		arg1 int
+		arg2 int
+		want int
+	}{
+		{
+			arg1: 1,
+			arg2: 2,
+			want: 3,
+		},
+		{
+			arg1: 2,
+			arg2: -2,
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := Add(tt.arg1, tt.arg2)
+			if got != tt.want {
+				t.Errorf("got: %v, want: %v\n", got, tt.want)
+			}
+		})
+	}
+}
