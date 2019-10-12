57a90267a0e8205f8b4756a8792e48591c7a5135
pomadev <y.pomadev@gmail.com>
2019-10-13 02:35:18 +0900

    go mod

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 // Add adds a and b, and returns the result
 func Add(a, b int) int {
 	return a + b
 }
 
+type config struct {
+	Host struct {
+		Address string `mapstructure:"address"`
+		Port    int    `mapstructure:"port"`
+	} `mapstructure:"host"`
+}
+
 func main() {
 	fmt.Println(Add(1, 1))
+
+	viper.SetDefault("host.address", "default")
+	viper.SetDefault("host.port", 1234)
+
+	viper.SetConfigFile("config.json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var c config
+	err = viper.Unmarshal(&c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c.Host.Address, c.Host.Port)
 }
